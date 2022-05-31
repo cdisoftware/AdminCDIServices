691b5e344f1aed19e80d24735cd1e3862a144515
Juan José Gutiérrez Leguizamón <68406983+JuanGut06@users.noreply.github.com>
2022-05-31 10:28:27 -0500

    NUEVOS SERVICIOS

---
+++ src/main/java/com/cdi/com/AdministradorCDI/Services/ConsultaListaTareaService.java
@@ -0,0 +1,9 @@
+package com.cdi.com.AdministradorCDI.Services;
+
+import com.cdi.com.AdministradorCDI.Entity.ConsultaListaTareaEntity;
+import java.util.List;
+
+public interface ConsultaListaTareaService {
+
+    public List<ConsultaListaTareaEntity> ConsultaListaTareas();
+}
--- src/main/java/com/cdi/com/AdministradorCDI/Services/EliminaUsuarioService.java
+++
@@ -1,8 +0,0 @@
-package com.cdi.com.AdministradorCDI.Services;
-
-import com.cdi.com.AdministradorCDI.Entity.EliminaUsuarioEntity;
-
-public interface EliminaUsuarioService {
-
-    public String EliminaUsuario(EliminaUsuarioEntity entidad, Integer bandera);
-}
